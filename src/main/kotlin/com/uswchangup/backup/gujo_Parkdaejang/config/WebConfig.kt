2bc6be9c558e859f20d829f00f326117c678b332
YooRee <yoori6260@naver.com>
2025-06-10 01:10:47 +0900

    fix: update frontend URL

--- src/main/kotlin/com/uswchangup/backup/gujo_Parkdaejang/config/WebConfig.kt
+++ src/main/kotlin/com/uswchangup/backup/gujo_Parkdaejang/config/WebConfig.kt
@@ -10,8 +10,8 @@ class WebConfig : WebMvcConfigurer {
 
     override fun addCorsMappings(registry: CorsRegistry) {
         registry.addMapping("/**")
-            .allowedOrigins("http://127.0.0.1:5500",
-                "http://localhost:5500")
+            .allowedOrigins("https://gujopark.netlify.app",
+                "https://gujopark.netlify.app")
             .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
             .allowedHeaders("*")
             .allowCredentials(true)
